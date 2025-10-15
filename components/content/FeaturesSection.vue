18e3ddcf68433c37b8a0603492250ff78b29dbaf
ehfazadnan <ehfazadnan.cse@gmail.com>
2025-10-15 12:46:32 +0600

    docs - remove unnecessary img place-holders

--- components/content/FeaturesSection.vue
+++ components/content/FeaturesSection.vue
@@ -12,12 +12,12 @@
             <div class="mb-6 flex h-20 w-20 items-center justify-center rounded-2xl bg-primary/10 p-3 group-hover:bg-primary/20 transition-colors">
               <template v-if="feature?.icon">
                 <NuxtImg
-                    :src="feature.icon.light || '/placeholder-icon-light.png'"
+                    :src="feature.icon.light"
                     :alt="feature.icon.alt || feature.title"
                     class="h-10 w-10 object-contain dark:hidden"
                 />
                 <NuxtImg
-                    :src="feature.icon.dark || feature.icon.light || '/placeholder-icon-dark.png'"
+                    :src="feature.icon.dark || feature.icon.light"
                     :alt="feature.icon.alt || feature.title"
                     class="hidden h-10 w-10 object-contain dark:block"
                 />
